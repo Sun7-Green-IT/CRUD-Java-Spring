8a2f5698026d77cb7bcb5ed92b9367d75feaaa0f
LevFlavien <levesque.flavien@outlook.fr>
2018-12-07 13:29:14 +0100

    Add fibonacci

--- src/main/java/com/example/postgresdemo/controller/IndexController.java
+++ src/main/java/com/example/postgresdemo/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.example.postgresdemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +14,20 @@ public class IndexController {
     @GetMapping("/helloworld")
     public String hello() {
         return "hello world";
+    }
+
+    @GetMapping("/fibonacci/{n}")
+    public long fibonacci(@PathVariable(value = "n") long n) {
+        if (n < 2) {
+            return(n);
+        }
+        return (fib(n-2) + fib(n-1));
+    }
+
+    private static long fib(long n) {
+        if (n < 2) {
+            return(n);
+        }
+        return (fib(n-2) + fib(n-1));
     }
 }
